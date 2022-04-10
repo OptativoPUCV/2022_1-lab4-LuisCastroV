5354f85f7dd9cb09520ccfc7ac6e2caf286c28ec
LuisCastroV <>
2022-04-10 16:42:51 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -87,7 +87,7 @@ void enlarge(HashMap * map)
   map->size=0;
   for(int i=0;i<map -> capacity;i++)
   {
-    if(sizeNuevo == map->size)
+    if(map->size == sizeNuevo)
     {
       if(Auxiliar[i]->key != NULL)
       {
