9f4f6007062f38b1d052a7b223d373b893d7eeba
LuisCastroV <>
2022-04-10 03:28:31 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -71,7 +71,7 @@ void insertMap(HashMap * map, char * key, void * value)
 void enlarge(HashMap * map)
 {
   enlarge_called = 1; //no borrar (testing purposes)
-  Pair** Auxiliar=map->buckets;
+  //Pair** Auxiliar=map->buckets;
   long CapacidadNueva = map->capacity*2;
   printf("%l",CapacidadNueva);
   
