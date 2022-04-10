98434d0acfe7b2a685cb0ab4ee08c94ce5c7bdab
LuisCastroV <>
2022-04-10 02:48:02 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -54,7 +54,7 @@ int is_equal(void* key1, void* key2){
 void insertMap(HashMap * map, char * key, void * value)
 {
   Pair *dato= createPair(key,value);
-  int index=hash(key,map->capacity);
+  int index=hash(dato,map->capacity);
   map->current = index;
   while(map->buckets[index]!=NULL)
   {
