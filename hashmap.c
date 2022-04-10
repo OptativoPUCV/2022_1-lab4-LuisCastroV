a22794bfeb5b0c796387c415f28cd6d7529ef0b1
LuisCastroV <>
2022-04-10 04:48:45 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -83,7 +83,7 @@ void enlarge(HashMap * map)
     {
       continue;
     }
-    if(Auxiliar[i]->key != NULL)
+    if(Auxiliar[i]->key == NULL)
     {
        insertMap(map,Auxiliar[i]->key, Auxiliar[i]->value);
      }
