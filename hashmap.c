0f4ff9a3f61880aa0715414ac3f5acf3aab40cdb
LuisCastroV <>
2022-04-10 03:25:15 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -71,7 +71,7 @@ void insertMap(HashMap * map, char * key, void * value)
 void enlarge(HashMap * map)
 {
   enlarge_called = 1; //no borrar (testing purposes)
-  Pair** Auxiliar=map->buckets;
+  //Pair** Auxiliar=map->buckets;
   
   
 }
