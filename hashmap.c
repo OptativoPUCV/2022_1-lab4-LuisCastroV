6b46129340bd36242865b8ca6bc685ce2f74e0a3
LuisCastroV <>
2022-04-10 04:06:53 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -76,9 +76,9 @@ void enlarge(HashMap * map)
   map=createMap(CapacidadNueva);
   for(int i=0;i<CapacidadNueva;i++)
   {
-    if(Auxiliar[i] == NULL)
+    if(Auxiliar[i] != NULL)
     {
-      if(Auxiliar[i]->key == NULL)
+      if(Auxiliar[i]->key != NULL)
       {
         insertMap(map,Auxiliar[i]->key, Auxiliar[i]->value);
       }
