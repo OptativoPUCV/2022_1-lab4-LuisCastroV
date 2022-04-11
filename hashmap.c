dbbf665e3cae7681db8f40e28f24669105c89495
LuisCastroV <>
2022-04-11 03:49:40 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -42,7 +42,6 @@ void insertMap(HashMap * map, char * key, void * value)
 {
   Pair *dato= createPair(key,value);
   int index=hash(dato->key,map->capacity);
-  //map->current = index;
   
   while(map->buckets[index]!=NULL)
   {
